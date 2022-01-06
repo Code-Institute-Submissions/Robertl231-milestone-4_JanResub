a527fa7e5bdb4cb3c6a4de25e12b39fbd1779653
Robertl231 <rloughran67@gmail.com>
2022-01-06 15:01:21 +0000

    added stripe to website

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KEwzcAMxik52sBxgsmD6AZ0JHPZUy4TZ7vflv0Z8xU66rvoExTMQblwT5KuwJ8e0YKoBqPh8raxtl6AweH389jk00cGP004cX',
+        'client_secret': 'test client secret',
     }
 
     return render (request, template, context)
