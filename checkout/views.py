17473a200b2d669ae912fa5ba4fa9b3d3531bd53
Robertl231 <rloughran67@gmail.com>
2022-01-07 02:36:30 +0000

    created payment intent in the checkout view

--- checkout/views.py
+++ checkout/views.py
@@ -10,6 +10,9 @@ import stripe
 # Create your views here.
 
 def checkout(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "Your bag is currently empty!")
@@ -18,13 +21,22 @@ def checkout(request):
     current_bag = bag_contents(request)
     total = current_bag['sub_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
 
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51KEwzcAMxik52sBxgsmD6AZ0JHPZUy4TZ7vflv0Z8xU66rvoExTMQblwT5KuwJ8e0YKoBqPh8raxtl6AweH389jk00cGP004cX',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render (request, template, context)
