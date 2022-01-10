9cbbf742ff914c48deeaca463e38adb955b605b8
Robertl231 <rloughran67@gmail.com>
2022-01-10 02:54:55 +0000

    fixe typing errors in profiles model.py that was casuing login issues. user in now able to go to profile

--- profiles/models.py
+++ profiles/models.py
@@ -6,14 +6,14 @@ from django.dispatch import receiver
 
 # Create your models here.
 class UserProfile(models.Model):
-    defTrueuser = models.OneToOneField(User, on_delete=models.CASCADE)
-    defTruephone_number = models.CharField(max_length=20, null=True, blank=True)
-    defTruecountry = CountryField(blank_label='Country *', null=True, blank=True)
-    defTruepostcode = models.CharField(max_length=20, null=True, blank=True)
-    defTruetown_or_city = models.CharField(max_length=40, null=True, blank=True)
-    defTruestreet_address1 = models.CharField(max_length=80, null=True, blank=True)
-    defTruestreet_address2 = models.CharField(max_length=80, null=True, blank=True)
-    defTruecounty = models.CharField(max_length=80, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(max_length=20, null=True, blank=True)
+    default_country = CountryField(blank_label='Country *', null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_town_or_city = models.CharField(max_length=40, null=True, blank=True)
+    default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
+    default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -23,5 +23,4 @@ def create_or_update_user_profile(sender, instance, created, **kwargs):
 
     if created:
         UserProfile.objects.create(user=instance)
-
     instance.userprofile.save()
