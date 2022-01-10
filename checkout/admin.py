8f9a42f31e4af120d0c29453810a8af2b13f3354
Robertl231 <rloughran67@gmail.com>
2022-01-10 00:11:31 +0000

    added more redundancys for stripe

--- checkout/admin.py
+++ checkout/admin.py
@@ -10,10 +10,10 @@ class OrderLineItemAdminInline(admin.TabularInline):
 class OrderAdmin(admin.ModelAdmin):
     inlines = (OrderLineItemAdminInline,)
 
-    readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'sub_total',)
+    readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'sub_total', 'original_bag', 'stripe_pid')
 
     field = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode',
-              'town_or_city', 'street_address1', 'street_address2', 'country', 'delivery_cost', 'order_total', 'sub_total', )
+              'town_or_city', 'street_address1', 'street_address2', 'country', 'delivery_cost', 'order_total', 'sub_total', 'original_bag', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name', 'order_total', 'delivery_cost', 'sub_total', )
 
