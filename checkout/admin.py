d387294fcdb4db98d29052b2991cf85f02a5a8b9
Robertl231 <rloughran67@gmail.com>
2022-01-10 04:46:44 +0000

    updated checkout views and users info is now saved from previous orders

--- checkout/admin.py
+++ checkout/admin.py
@@ -12,7 +12,7 @@ class OrderAdmin(admin.ModelAdmin):
 
     readonly_fields = ('order_number', 'date', 'delivery_cost', 'order_total', 'sub_total', 'original_bag', 'stripe_pid')
 
-    field = ('order_number', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode',
+    field = ('order_number', 'user_profile', 'date', 'full_name', 'email', 'phone_number', 'country', 'postcode',
               'town_or_city', 'street_address1', 'street_address2', 'country', 'delivery_cost', 'order_total', 'sub_total', 'original_bag', 'stripe_pid')
 
     list_display = ('order_number', 'date', 'full_name', 'order_total', 'delivery_cost', 'sub_total', )
