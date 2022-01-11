1a83eb0904375339bf4ddf3c635423036737b60c
Robertl231 <rloughran67@gmail.com>
2022-01-11 22:59:43 +0000

    added functionality to delete products

--- products/views.py
+++ products/views.py
@@ -68,9 +68,9 @@ def add_product(request):
     if request.method =='POST':
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
-            form.save()
+            product = form.save()
             messages.success(request, 'You have successfully added a product!')
-            return redirect(reverse('add_product'))
+            return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Unabel to add product, please ensure form is valid.')
     else:
@@ -104,3 +104,9 @@ def edit_product(request, product_id):
     }
 
     return render(request, template, context)
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, 'Product has been deleted!')
+    return redirect(reverse('products'))
