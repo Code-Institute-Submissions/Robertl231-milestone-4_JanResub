bd77cbb41f8cf3af4dde86742aab6da5b1ff8029
Robertl231 <rloughran67@gmail.com>
2022-01-12 04:17:39 +0000

    removed secret key from settings.py

--- game_hut/settings.py
+++ game_hut/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$tjodp1+6y=913qw!8-6=o@@cavuvomoiq#-wmh@_=jmkimpp'
-#os.environ.get('SECRET_KEY', '')
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEVELOPMENT' in os.environ
@@ -136,7 +135,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-#'NAME': BASE_DIR / 'db.sqlite3',
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
